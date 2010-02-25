917c7df6f439e245e6351ec1d5da5bbf233746fc
sungon@gmail.com <sungon@gmail.com@33f6148c-21a6-11df-9e09-9f768d97ad4a>
2010-02-25 12:02:28 +0000

    core migrations in place

--- fail/dynamic/app/fail-migrations.js
+++ fail/dynamic/app/fail-migrations.js
@@ -1,5 +1,13 @@
 var dbm = require("db-migrations");
 
-exports.migrate = function() {
-	
+dbm.version(0, "initial", function(con) {
+	con.update("CREATE TABLE fails (id SERIAL, name VARCHAR(255), PRIMARY KEY (id))");
+});
+
+dbm.version(1, "fail-description", function(con) {
+	con.update("ALTER TABLE fails ADD description VARCHAR(1024)");
+});
+
+exports.migrate = function(con) {
+	dbm.migrate(con);
 }
