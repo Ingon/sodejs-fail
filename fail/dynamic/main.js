b1bcf910e28b16f2a2f0e25919441c17c10071fc
sungon@gmail.com <sungon@gmail.com@33f6148c-21a6-11df-9e09-9f768d97ad4a>
2010-02-26 00:11:51 +0000

    its alive

--- fail/dynamic/main.js
+++ fail/dynamic/main.js
@@ -3,6 +3,10 @@ require("fail-migrations").migrate(migrationConn);
 migrationConn.close();
 
 var con = require("db-core").pgConnect("fail");
+
+var dao = require("dao").init(con);
+dao.register("fails");
+
 try {
 	require("fail-actions").execute();
 } finally {
